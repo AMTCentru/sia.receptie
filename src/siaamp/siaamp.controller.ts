1b22d78238e6a8b6b4eec80dae0e6a0eb45bae13
TheMotans <stoica.adrian1997@gmail.com>
2024-12-18 17:29:37 +0200

    v1.3

--- src/siaamp/siaamp.controller.ts
+++ src/siaamp/siaamp.controller.ts
@@ -1,11 +1,14 @@
 import { siaampService } from './siaamp.service';
 import { Response } from 'express';
-import { Controller, Get, Query, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Query, Res, HttpStatus, Logger, HttpException } from '@nestjs/common';
 import { ApiTags, ApiQuery, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @ApiTags('siaReceptie') // Gruparea endpoint-urilor în Swagger
 @Controller('siaReceptie')
 export class TransfersiaController {
+
+  private readonly logger = new Logger(TransfersiaController.name)
+
   constructor(private service: siaampService) {}
 
   @Get('logareSiaReceptie')
@@ -18,10 +21,8 @@ export class TransfersiaController {
       res.setHeader('Content-Type', 'image/png'); // Setăm tipul de conținut
       res.status(HttpStatus.OK).send(data); // Răspuns cu bufferul imaginii
     } catch (error) {
-      console.error('Error during login:', error);
-      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-        message: 'An error occurred during login.',
-      });
+      this.logger.error(`[logareSiaReceptie] : ${error}`);
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'An error occurred during login.',});
     }
   }
 
@@ -33,7 +34,7 @@ export class TransfersiaController {
       res.setHeader('Content-Type', 'image/png'); // Set the content type for a PNG image
       res.status(HttpStatus.OK).send(image); // Send the image buffer as response
     } catch (error) {
-      //this.logger.error('[checkLogatClientSIAAMP]', error);
+      this.logger.error('[checkLogatClientSIAAMP]', error);
       res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(error);
     }
   }
@@ -44,10 +45,41 @@ export class TransfersiaController {
   @ApiQuery({ name: 'stopdata', type: String, description: 'Data de sfârșit (format YYYY-MM-DD)' })
   @ApiResponse({ status: 200, description: 'Procesul a fost pornit cu succes.' })
   @ApiResponse({ status: 500, description: 'Eroare în timpul procesului.' })
-  async startSiaReceptie(
+  startSiaReceptie(
     @Query('startdata') startdata: string,
     @Query('stopdata') stopdata: string,
   ) {
-    return await this.service.startSiaReceptie(startdata, stopdata);
+    this.service.startSiaReceptie(startdata, stopdata);
+  }
+
+  @Get('screenshots')
+  @ApiResponse({
+      status: 200,
+      description: 'Screenshot-uri capturate cu succes.',
+  })
+  @ApiResponse({
+      status: 500,
+      description: 'Eroare la capturarea screenshot-urilor.',
+  })
+  async getScreenshots(@Res() res: Response) {
+      try {
+          const screenshots = await this.service.captureAllPages();
+
+          const result = screenshots.map((screenshot) => ({
+              pageIndex: screenshot.pageIndex,
+              url: screenshot.url,
+              image: screenshot.screenshotBuffer.toString('base64'),
+          }));
+
+          return res.status(HttpStatus.OK).json({
+              success: true,
+              pages: result,
+          });
+      } catch (error) {
+          throw new HttpException(
+              { message: 'Eroare la capturarea screenshot-urilor', details: error.message },
+              HttpStatus.INTERNAL_SERVER_ERROR,
+          );
+      }
   }
 }
--- src/siaamp/siaamp.service.ts
+++ src/siaamp/siaamp.service.ts
@@ -12,17 +12,27 @@ export class siaampService {
     constructor(private siaservice: SiaampReceptieService) {}
 
     async logareSiaReceptie(): Promise<Buffer> {
-        try {
-            console.log('Launching Puppeteer...');
-            this.browser = await puppeteer.launch({
-                executablePath: '/usr/bin/google-chrome-stable',
-                headless: false,
-                //executablePath: '/usr/bin/chromium-browser',
-                args: ['--no-sandbox', '--disable-setuid-sandbox'],
-            });
+        if (this.browser) {
+            this.logger.error('[logareSiaReceptie] Exista browser pornit');
 
-            const page = await this.browser.newPage();
-            await page.goto('https://sia.amp.md/siaamp/');
+            throw new Error('[logareSiaReceptie] Exista browser pornit');
+        }
+        this.logger.log('Launching Puppeteer...');
+        this.browser = await puppeteer.launch({
+            executablePath: '/usr/bin/google-chrome-stable',
+            //headless: false,
+            //executablePath: '/usr/bin/chromium-browser',
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        });
+
+        const pages: Page[] = await this.browser.pages();
+        const page: Page = pages[0]; // Prima pagină deschisă
+        try {
+            await page.goto('https://sia.amp.md/siaamp/',{
+                waitUntil:'load',
+                timeout: 5000
+            });
+            this.logger.log(`[logareSiaReceptie] Pagina Deschisa`)
 
             const butonLogare = "#formMPass > div > div:nth-child(2) > a"
             await page.waitForSelector(butonLogare)
@@ -53,21 +63,22 @@ export class siaampService {
 
         } catch (error) {
             await this.browser.close()
+            this.browser = null
             this.logger.error('[logareSiaReceptie] :', error);
             throw new BadRequestException('An error occurred while processing your request.');
-        } 
+        }
     }
 
     async checkLogat(){
+        if (!this.browser) {
+            throw new Error('Nu exista browser pornit');
+        }
+        const page = await this.browser.newPage();
+        this.logger.error('[checkLogat] Pagina deschisa');
         try{
-            if (!this.browser) {
-                throw new Error('Nu exista browser pornit');
-            }
-            const page = await this.browser.newPage();
             await page.goto('https://sia.amp.md/siaamp/mpassUsers.html');
-
             const opertorRegistratura = ".ui-datagrid-row>td:nth-child(2)>div>div>form>input:nth-child(6)"
-            await page.waitForSelector(opertorRegistratura)
+            await page.waitForSelector(opertorRegistratura,{timeout:5000})
             const filePath = `${process.env.LOG_LOCATION}/screenshot.png`;
             if (!opertorRegistratura) {
                 await page.screenshot({path: filePath,})
@@ -76,8 +87,7 @@ export class siaampService {
             }  
             await page.click(opertorRegistratura)
 
-            await page.goto('https://sia.amp.md/siaamp/',{
-                waitUntil: 'load', // Așteaptă încărcarea completă
+            await page.goto('https://sia.amp.md/siaamp/',{waitUntil: 'load', // Așteaptă încărcarea completă
             });
 
             await page.screenshot({path: filePath,})
@@ -86,15 +96,20 @@ export class siaampService {
         } catch (error) {
             if (this.browser) {
                 await this.browser.close();  // Închide browserul doar dacă este deschis
+                this.browser = null
             }
             this.logger.error('[checkLogat]', error);
             throw new BadRequestException(error);
-        } 
+        } finally {
+            await page.close()
+            this.logger.error('[checkLogat] Pagina inchisa');
+        }
     }
 
     async startSiaReceptie(startdata: string, stopdata: string){
 
-        const page : Page = await this.browser.newPage()
+        const pages: Page[] = await this.browser.pages();
+        const page: Page = pages[0]; // Prima pagină deschisă
         
         try {
             await this.siaservice.Pas1(page)
@@ -105,7 +120,44 @@ export class siaampService {
         } 
         catch (error) {
             await this.browser.close()
-            return `Error while scraping job listings: ${error}`;
+            this.browser = null
+            this.logger.error(`[startSiaReceptie] eroare : ${error}`);
         } 
     }
+
+    async captureAllPages() {
+        try {
+            if (!this.browser) {
+                throw new Error('Browser-ul nu este pornit.');
+            }
+    
+            // Obține toate paginile deschise din browser
+            const pages = await this.browser.pages();
+            if (!pages.length) {
+                throw new Error('Nu există pagini deschise în browser.');
+            }
+    
+            const screenshots = [];
+            const filePath = `${process.env.LOG_LOCATION}`;
+    
+            // Iterează prin fiecare pagină și capturează un screenshot
+            for (const [index, page] of pages.entries()) {
+                const screenshotPath = `${filePath}/screenshot_page_${index + 1}.png`;
+                await page.screenshot({ path: screenshotPath, fullPage: true });
+    
+                // Opțional: citește bufferul screenshot-ului pentru a returna
+                const screenshotBuffer = await readFile(screenshotPath);
+                screenshots.push({
+                    pageIndex: index + 1,
+                    url: page.url(),
+                    screenshotBuffer,
+                });
+            }
+    
+            return screenshots;
+        } catch (error) {
+            this.logger.error('[captureAllPages]', error);
+            throw new BadRequestException(error);
+        }
+    }   
 }
